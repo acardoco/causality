c14b09c304719efd81a5f07b9e0748a085ddeb26
acardoco <mrsugus12@gmail.com>
2019-07-27 16:50:48 +0200

    añadido web scraping de los 400 filtrado por fechas

--- data_manager.py
+++ data_manager.py
@@ -3,40 +3,58 @@ import pandas as pd
 import yfinance as yf
 import requests
 from bs4 import BeautifulSoup
+import datetime
+import time
+
+COLUMNAS = ['Open', 'High', 'Low', 'Close', 'Volume']
 
 def get_SP_400():
 
-    website = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_400_companies')
+    website = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_400_companies').text
+    soup = BeautifulSoup(website, 'lxml')
+    tabla = soup.find('table', {'class': 'wikitable sortable'})
+    lista_tickers = tabla.findAll("a", {"class": "external text"})
 
-    soup = BeautifulSoup(website, 'html')
+    lista_final_sticker = []
+    for ticker in lista_tickers:
+        t = ticker.string
+        if t != 'reports':
+            lista_final_sticker.append(t)
+
+    print(len(lista_final_sticker))
+    return np.asarray(lista_final_sticker)
 
 
+def get_ticker_info(ticker, cols, start=datetime.datetime(2017,1,1), end=datetime.datetime(2018,1,1)):
+
+    try:
+        ticker_info = yf.Ticker(ticker)
+
+        # get historical market data
+        hist = ticker_info.history(period="max")
+        hist = hist[(hist.index >= start) & (hist.index <= end)]
+
+        return hist[cols]
+    except ValueError:
+        print(ticker + ' does not exists.')
+        return None
 
 
+def save_info():
 
-def get_ticker_info(ticker, cols, start="2017-01-01", end="2018-01-01"):
+    list400 = get_SP_400()
 
-    ticker_info = yf.Ticker(ticker)
-
-    # get stock info
-    stock = ticker_info.info
-
-    # get historical market data
-    hist = ticker_info.history(period="max")
-    hist = hist.reset_index()
-
-    hist = hist[
-        (hist['Date'] >= start) &
-        (hist['Date'] <= end) ]
-    return hist[cols], stock
-
-def save_info(list400):
-
-    cols = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+    i = 0
+    start_time = time.time()
     for ele in list400:
-        info = get_ticker_info('ticker', cols)
+        print(ele)
+        info = get_ticker_info(ele, COLUMNAS)
+        i+=1
+        print(i)
+    end_time = time.time()
+    print('Download info time:', end_time-start_time)
 
-hist, stock = get_ticker_info('ACHC',['Date', 'Open', 'High', 'Low', 'Close', 'Volume'],'','')
-print(stock)
-print(hist.head())
+#hist, stock = get_ticker_info('ACHC',COLUMNAS,'','')
+#print(hist.head())
 #get_SP_400()
+save_info()
