312c0ea537fcf45b7c621da0d1c0e24b21577b8f
acardoco <mrsugus12@gmail.com>
2019-07-27 14:32:35 +0200

    cambios pequeños

--- data_manager.py
+++ data_manager.py
@@ -14,7 +14,7 @@ def get_SP_400():
 
 
 
-def get_ticker_info(ticker, cols, start, end):
+def get_ticker_info(ticker, cols, start="2017-01-01", end="2018-01-01"):
 
     ticker_info = yf.Ticker(ticker)
 
@@ -23,10 +23,20 @@ def get_ticker_info(ticker, cols, start, end):
 
     # get historical market data
     hist = ticker_info.history(period="max")
+    hist = hist.reset_index()
 
-    return hist[['Open', 'High', 'Low', 'Close', 'Volume']], stock
+    hist = hist[
+        (hist['Date'] >= start) &
+        (hist['Date'] <= end) ]
+    return hist[cols], stock
 
-'''hist, stock = get_ticker_info('ACHC','','','')
+def save_info(list400):
+
+    cols = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+    for ele in list400:
+        info = get_ticker_info('ticker', cols)
+
+hist, stock = get_ticker_info('ACHC',['Date', 'Open', 'High', 'Low', 'Close', 'Volume'],'','')
 print(stock)
-print(hist.head())'''
-get_SP_400()
+print(hist.head())
+#get_SP_400()
